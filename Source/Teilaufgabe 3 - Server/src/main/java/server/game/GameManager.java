eb68b28360af685f9cf83ff6bc533d38fd359375
jardam99 <a11921801@unet.univie.ac.at>
2022-06-07 01:58:49 +0200

    Green - addGame method in GameManager

--- Source/Teilaufgabe 3 - Server/src/main/java/server/game/GameManager.java
+++ Source/Teilaufgabe 3 - Server/src/main/java/server/game/GameManager.java
@@ -1,7 +1,10 @@
 package server.game;
 
+import server.exceptions.GameAlreadyExistsException;
 import server.map.MapManager;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -9,7 +12,22 @@ public class GameManager {
     private Map<GameID, MapManager> gameManagers;
     private Set<GameClass> games;
 
-    public void addGame(GameID gameID){};
+    public GameManager(){
+        games = new HashSet<>();
+        gameManagers = new HashMap<>();
+    }
+
+    public void addGame(GameID gameID){
+        boolean gameExists = games
+                .stream()
+                .anyMatch(g -> g.getGameID().equals(gameID));
+
+        if(gameExists)
+            throw new GameAlreadyExistsException("This game id is already registered");
+
+        GameClass game = new GameClass(gameID);
+        games.add(game);
+    };
     public GameClass getGameWithID(){
         return null;
     }
