a3020329ff84808ac91d37a31d432a89cc6e95ff
jardam99 <a11921801@unet.univie.ac.at>
2022-06-06 23:52:27 +0200

    Red - Implement GameClass

--- Source/Teilaufgabe 3 - Server/src/test/java/server/game/GameClassTest.java
+++ Source/Teilaufgabe 3 - Server/src/test/java/server/game/GameClassTest.java
@@ -1,7 +1,14 @@
 package server.game;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import server.exceptions.MapAlreadySet;
+import server.exceptions.NoPlayerRegistered;
+import server.exceptions.TwoPlayersAlreadyRegisteredExeception;
+import server.map.MapClass;
+import server.player.Player;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,16 +20,44 @@ class GameClassTest {
         game = new GameClass(id);
     }
     @Test
-    public void getRound_updateRoundNeverCalled_roundEqualZero(){}
+    public void getRound_updateRoundCalledOnce_roundEqualToOne(){
+        game.updateRound();
+        int expected = 1;
+        int actual = game.getRound();
+
+        Assertions.assertEquals(expected, actual);
+    }
     @Test
-    public void updateRound_updateOnce_roundEqualInitValuePlusOne() {}
+    public void updateRound_updateOnce_roundEqualInitValuePlusOne() {
+        int before = game.getRound();
+        int expectedAfter = before + 1;
+
+        game.updateRound();
+        int actualAfter = game.getRound();
+
+        Assertions.assertEquals(expectedAfter, actualAfter);
+    }
 
     @Test
-    public void registerPlayer_twoPlayersAlreadyRegistered_throwException(){}
+    public void registerPlayer_twoPlayersAlreadyRegistered_throwException(){
+        game.registerPlayer(new Player());
+        game.registerPlayer(new Player());
+
+        Executable registerPlayer = () -> game.registerPlayer(new Player());
+        Assertions.assertThrows(TwoPlayersAlreadyRegisteredExeception.class, registerPlayer);
+    }
 
     @Test
-    public void getPlayerWithID_idOfPlayerNotRegistered_throwException(){}
+    public void getPlayerWithID_idOfPlayerNotRegistered_throwException(){
+        Executable getPlayer = () -> game.getPlayerWithID("id");
+        Assertions.assertThrows(NoPlayerRegistered.class, getPlayer);
+    }
 
     @Test
-    public void setFullMap_fullMapAlreadySet_throwException() {}
+    public void setFullMap_fullMapAlreadySet_throwException() {
+        game.setFullMap(new MapClass());
+        Executable setMap = () -> game.setFullMap(new MapClass());
+
+        Assertions.assertThrows(MapAlreadySet.class, setMap);
+    }
 }
