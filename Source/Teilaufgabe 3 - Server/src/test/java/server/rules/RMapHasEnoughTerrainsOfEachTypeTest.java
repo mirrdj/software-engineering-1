e313450001c4025dc53eb2e8ab3b3853f84600d1
jardam99 <a11921801@unet.univie.ac.at>
2022-06-24 20:57:19 +0200

    Red RMapHasEnoughTerrainsOfEachType

--- Source/Teilaufgabe 3 - Server/src/test/java/server/rules/RMapHasEnoughTerrainsOfEachTypeTest.java
+++ Source/Teilaufgabe 3 - Server/src/test/java/server/rules/RMapHasEnoughTerrainsOfEachTypeTest.java
@@ -1,15 +1,56 @@
 package server.rules;
 
+import MessagesBase.MessagesFromClient.HalfMap;
+import MessagesBase.UniqueGameIdentifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import server.exceptions.WrongNumberOfFieldsException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RMapHasEnoughTerrainsOfEachTypeTest {
+    private RMapHasEnoughTerrainsOfEachType rule;
+    private MapCreator mapCreator = new MapCreator();
+    @BeforeEach
+    private void setUp(){
+        rule = new RMapHasEnoughTerrainsOfEachType();
+    }
     @Test
-    void validateHalfMap_notEnoughWaterFields_throwWrongNumberOfFieldsException() {
+    public void validateHalfMap_notEnoughWaterFields_throwWrongNumberOfFieldsException() {
+        char[][] nodes = {
+                {'G', 'G', 'G', 'M'},
+                {'G', 'G', 'G', 'M'},
+                {'G', 'W', 'G', 'M'},
+                {'G', 'W', 'G', 'M'},
+                {'G', 'W', 'G', 'G'},
+                {'G', 'G', 'G', 'G'},
+                {'G', 'G', 'G', 'G'},
+                {'G', 'G', 'G', 'G'},
+
+        };
+
+        UniqueGameIdentifier gameID = new UniqueGameIdentifier("game1");
+        HalfMap halfMap = mapCreator.createMap(nodes);
+
+        Executable checkFields = () -> rule.validateHalfMap(gameID, halfMap);
+        Assertions.assertThrows(WrongNumberOfFieldsException.class, checkFields);
     }
 
     @Test
-    void validateHalfMap_notEnoughMountainFields_throwWrongNumberOfFieldsException() {
+    public void validateHalfMap_notEnoughMountainFields_throwWrongNumberOfFieldsException() {
+        char[][] nodes = {
+                {'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
+                {'G', 'G', 'W', 'W', 'W', 'W', 'G', 'G'},
+                {'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
+                {'G', 'G', 'M', 'M', 'G', 'G', 'G', 'G'},
+        };
+
+        UniqueGameIdentifier gameID = new UniqueGameIdentifier("game1");
+        HalfMap halfMap = mapCreator.createMap(nodes);
+
+        Executable checkFields = () -> rule.validateHalfMap(gameID, halfMap);
+        Assertions.assertThrows(WrongNumberOfFieldsException.class, checkFields);
     }
 }
