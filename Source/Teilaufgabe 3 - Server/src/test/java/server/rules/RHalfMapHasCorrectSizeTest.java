0e8b87abf3af79930bfcbd6c040bcba3f8736e53
jardam99 <a11921801@unet.univie.ac.at>
2022-06-24 19:44:46 +0200

    Think RHalfMapHasCorrectSize
    RHalfMapHasCorrectSize checks whether the x value of the nodes are within 0 and 8 and the lowest are within 0 and 3

---
+++ Source/Teilaufgabe 3 - Server/src/test/java/server/rules/RHalfMapHasCorrectSizeTest.java
@@ -0,0 +1,24 @@
+package server.rules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RHalfMapHasCorrectSizeTest {
+    @Test
+    void validateHalfMap_highestXValueOfNode6_throwMapWrongSizeException() {
+    }
+
+    @Test
+    void validateHalfMap_highestXValueOfNode8_throwMapWrongSizeException() {
+    }
+
+    @Test
+    void validateHalfMap_highestYValueOfNode2_throwMapWrongSizeException() {
+    }
+
+    @Test
+    void validateHalfMap_highestYValueOfNode4_throwMapWrongSizeException() {
+    }
+
+}
