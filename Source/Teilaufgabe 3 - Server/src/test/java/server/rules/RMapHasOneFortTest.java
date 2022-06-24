0e488b3b5623c3ff996eb28d0899a75fe06e345d
jardam99 <a11921801@unet.univie.ac.at>
2022-06-24 21:32:50 +0200

    Red RMapHasOneFort

--- Source/Teilaufgabe 3 - Server/src/test/java/server/rules/RMapHasOneFortTest.java
+++ Source/Teilaufgabe 3 - Server/src/test/java/server/rules/RMapHasOneFortTest.java
@@ -1,15 +1,42 @@
 package server.rules;
 
+import MessagesBase.MessagesFromClient.HalfMap;
+import MessagesBase.UniqueGameIdentifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import server.exceptions.MapHasWrongSizeException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RMapHasOneFortTest {
-    @Test
-    void validateHalfMap_halfMapHasNoFort_throwWrongFortNumberException() {
+    private MapCreator mapCreator = new MapCreator();
+    private RMapHasOneFort rule;
+
+    @BeforeEach
+    private void setUp(){
+        rule = new RMapHasOneFort();
+
     }
 
     @Test
-    void validateHalfMap_halfMapHasMoreThanOneFort_throwWrongFortNumberException() {
+    public void validateHalfMap_halfMapHasNoFort_throwWrongFortNumberException() {
+        boolean placeFort = false;
+        HalfMap halfMap = mapCreator.createMap( 4, 8, placeFort);
+        UniqueGameIdentifier gameID = new UniqueGameIdentifier("game1");
+
+        Executable validateSize = () -> rule.validateHalfMap(gameID, halfMap);
+        Assertions.assertThrows(MapHasWrongSizeException.class, validateSize);
+    }
+
+    @Test
+    public void validateHalfMap_halfMapHasMoreThanOneFort_throwWrongFortNumberException() {
+        boolean placeFort = false;
+        HalfMap halfMap = mapCreator.createMap( 4, 8, placeFort);
+        UniqueGameIdentifier gameID = new UniqueGameIdentifier("game1");
+
+        Executable validateSize = () -> rule.validateHalfMap(gameID, halfMap);
+        Assertions.assertThrows(MapHasWrongSizeException.class, validateSize);
     }
 }
